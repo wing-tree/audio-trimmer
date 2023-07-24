8d1e616f479eaf1c4fc1fa223dc40906d59c78cf
noah <noah@supergene.co>
2023-07-24 18:45:41 +0900

    Add divider

--- app/src/main/java/wing/tree/audio/trimmer/view/compose/composable/Trimmed.kt
+++ app/src/main/java/wing/tree/audio/trimmer/view/compose/composable/Trimmed.kt
@@ -3,8 +3,9 @@ package wing.tree.audio.trimmer.view.compose.composable
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,18 +25,27 @@ fun Trimmed(
         contentKey = {
             it::class
         },
-    ) { targetState ->
-        when (targetState) {
+    ) {
+        when (it) {
             TrimmedState.Loading -> Box(modifier = modifier) {
                 CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
             }
 
             is TrimmedState.Content -> LazyColumn(modifier = modifier) {
-                items(targetState.audioFiles) {
+                itemsIndexed(
+                    items = it.audioFiles,
+                    key = { _, item ->
+                        item.id
+                    }
+                ) { index, item ->
                     AudioFile(
-                        audioFile = it,
+                        audioFile = item,
                         onClick = onItemClick,
                     )
+
+                    if (index < it.audioFiles.lastIndex) {
+                        Divider()
+                    }
                 }
             }
 
